de5337517709a9e4346db3c153c26d2d5330287d
Mahesh <liquidscorpio@users.noreply.github.com>
2018-01-22 11:24:38 +0530

    #10 relative import for python less than 3

--- django_multitenant/__init__.py
+++ django_multitenant/__init__.py
@@ -1,1 +1,6 @@
-from django_multitenant.django_multitenant import *
+import sys
+
+if sys.version_info >= (3,0):
+    from django_multitenant.django_multitenant import *
+else:
+    from .django_multitenant import *
