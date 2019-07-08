c67a8e70640a3571f40cbfe8b711f3c92230b6f5
Ben Ledbury <ben@machinelabs.com>
2019-07-08 16:38:23 +0100

    Fix commented out code that shouldn't be

--- django_multitenant/query.py
+++ django_multitenant/query.py
@@ -6,15 +6,13 @@ from .utils import get_current_tenant, get_tenant_column, get_tenant_filters
 
 def wrap_get_compiler(base_get_compiler):
     def get_compiler(obj, *args, **kwargs):
-        # current_tenant = get_current_tenant()
-        # if current_tenant:
-        #     try:
-        #         filters = get_tenant_filters(obj.model)
-        #         obj.add_q(Q(
-        #             **filters
-        #         ))
-        #     except ValueError:
-        #         pass
+        current_tenant = get_current_tenant()
+        if current_tenant:
+            try:
+                filters = get_tenant_filters(obj.model)
+                obj.add_q(Q(**filters))
+            except ValueError:
+                pass
         return base_get_compiler(obj, *args, **kwargs)
 
     get_compiler._sign = "get_compiler django-multitenant"
