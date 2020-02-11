454898c3f722c09558aee4a2355096a4a38492e9
Louise Grandjonc <louve.grandjonc@gmail.com>
2020-02-11 13:04:33 -0500

    fix installed apps

--- django_multitenant/tests/settings.py
+++ django_multitenant/tests/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    "django.contrib.messages",
     "django.contrib.sites",
     "django_multitenant",
     "django_multitenant.tests",
