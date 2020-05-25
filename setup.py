b90bb58ed6f27b205a61395e2ffe198ada6879e3
Louise Grandjonc <louve.grandjonc@gmail.com>
2020-05-25 15:39:42 +0200

    release new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from io import open
 
 setup(
     name="django-multitenant",
-    version='2.2.1',  # Required
+    version='2.2.2',  # Required
     description='Django Library to Implement Multi-tenant databases',
     url="https://github.com/citusdata/django-multitenant",
     author='Louise Grandjonc',
