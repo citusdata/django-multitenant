98fc0168c9d20d39511af53d5a430dea491ac073
Louise Grandjonc <louve.grandjonc@gmail.com>
2020-08-24 14:10:40 +0200

    add comments and exception on django 3.0

--- django_multitenant/deletion.py
+++ django_multitenant/deletion.py
@@ -8,12 +8,14 @@ from .utils import (get_current_tenant, get_tenant_filters)
 
 
 def related_objects(obj, *args):
-    if django.VERSION[0] < 3:
+    if django.VERSION[0] < 3 or len(args) == 2:
         related = args[0]
         related_model = related.related_model
         related_fields = [related.field]
         objs = args[1]
     else:
+        # Starting django 3.1 the signature of related_objects changed to
+        # def related_objects(self, related_model, related_fields, objs)
         related_model = args[0]
         related_fields = args[1]
         objs = args[2]
