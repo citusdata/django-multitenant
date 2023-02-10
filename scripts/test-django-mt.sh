65cd95db06aa51ea9f3f94c4efa80fe1d065d771
Gurkan Indibay <gindibay@microsoft.com>
2023-02-10 11:31:40 +0300

    Addresses review items

--- scripts/test-django-mt.sh
+++ scripts/test-django-mt.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 pyenv global "${PYTHON_VERSION}"
-echo "${PATH}"
-pyenv global "${PYTHON_VERSION}"
 echo "Python version: $(python --version)"
-pip install -r /build/requirements/test-requirements.txt 
-pip install Django=="${DJANGO_VERSION}"
+make dev-dependencies
 cd /build || exit
 make test
