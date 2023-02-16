90f6fd437c9ff0ae6eb8eb4414d67100099e64d9
Gurkan Indibay <gindibay@microsoft.com>
2023-02-16 06:59:29 +0300

    Reformats files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ release = "3.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinxnotes.strike']
+extensions = ["sphinxnotes.strike"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
