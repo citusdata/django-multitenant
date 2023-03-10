6fdea3b94377b3e1898b5646e3e8cf2d075790aa
Gurkan Indibay <gindibay@microsoft.com>
2023-03-10 14:46:32 +0300

    Deletes unnecessary pip install

--- scripts/test-django-mt-modules.sh
+++ scripts/test-django-mt-modules.sh
@@ -3,7 +3,5 @@ pyenv global "${PYTHON_VERSION}"
 echo "Python version: $(python --version)"
 cd /build || exit
 make test-dependencies
-if [[ "${DJANGO_REST_FRAMEWORK}" == "true" ]]; then
-    pip install djangorestframework
-fi
+
 make test-missing-modules
