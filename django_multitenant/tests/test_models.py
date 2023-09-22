9188bd38685f8dadc90cc3f4363bf5f9b8d8118a
gindibay <gindibay@microsoft.com>
2023-09-22 13:03:53 +0300

    Fixes static code analysis issues

--- django_multitenant/tests/test_models.py
+++ django_multitenant/tests/test_models.py
@@ -311,7 +311,7 @@ class TenantModelTest(BaseTestCase):
 
             for query in captured_queries.captured_queries:
                 print(f"SQL: {query['sql']}")
-                
+
                 if query["sql"] == "BEGIN":
                     continue
                 if "tests_revenue" in query["sql"]:
